82cc4a4715a6efaf9fd2ae3e699669d3fbbdf7e3
lucasgonzalezgagliardo <lucas.e.gonzalez@gmail.com>
2023-03-02 19:31:16 +0100

    tweetssss

--- src/App.jsx
+++ src/App.jsx
@@ -34,10 +34,15 @@ const tweetsArray = [
   },
 ];
 
+
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {
+        tweetsArray.map((tweet) => (
+          <Tweet tweet={tweet} />
+        ))
+      }
     </div>
   );
 }
